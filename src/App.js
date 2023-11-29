e8898b7e108dd0426f571a7d0cdc1ccab64a33a4
MohammedMujahidAhmed <mujahid13131@gmail.com>
2023-11-30 00:04:20 +0530

    Added team profile slider components-Cards,Members by mujahid

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import AboutTourism from './Components/AboutTourism';
 import { AboutTuar } from './Components/AboutTuar';
+import Cards from './Components/Cards';
 import HeroSection from './Components/HeroSection';
+import members from "./data"
 
 
 function App() {
@@ -10,6 +12,10 @@ function App() {
       <HeroSection/>
       <AboutTuar/>
       <AboutTourism/>
+      <div className='p-3 flex flex-col justify-center items-center'>
+        <p className='font-extrabold text-[28px]'>Our Team</p>
+        <Cards members={members}></Cards>
+      </div>
     </div>
   );
 }
